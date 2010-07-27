5c6bac1ae53cd27f3b38f23446ebf4ed107e6b3c
Louis Rose <louismrose@gmail.com>
2010-07-27 20:27:18 +0100

    Installed RSpec.

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -12,7 +12,7 @@ module NavigationHelpers
       '/'
       
     when /the tracker page/
-      '/'
+      '/entries/index'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
