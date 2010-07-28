19e1d0ebae0fe4d334727e321d37e02ab3b0b84b
Louis Rose <louismrose@gmail.com>
2010-07-28 17:24:19 +0100

    Added Entry.today

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,9 @@ class Entry
   include Mongoid::Document
   field :name, :type => String
   field :points, :type => Float
+  field :created, :type => Date
+  
+  def self.today
+    where(:created => Time.now.utc.midnight)
+  end
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Entry do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context "when today is called" do
+    it "should return only those entries created today" do
+      yesterday = create_entry(1.day.ago)
+      today     = create_entry(2.seconds.ago)
+      tomorrow  = create_entry(1.day.from_now)
+            
+      Entry.today.size.should == 1
+      Entry.today.first.should eq(today)
+    end
+  end
+  
+  def create_entry(creation_date)
+    entry = Entry.new(:created => creation_date.utc)
+    entry.save
+    entry
+  end
 end
